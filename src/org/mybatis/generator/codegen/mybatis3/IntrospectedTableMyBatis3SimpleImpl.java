ff204677dd1fe3856373314dc9bacd94e6050ac1
Belong.Zhang <zhangbin-510@163.com>
2018-02-05 17:40:12 +0800

    dis finish version

---
+++ src/org/mybatis/generator/codegen/mybatis3/IntrospectedTableMyBatis3SimpleImpl.java
@@ -0,0 +1,102 @@
+/**
+ *    Copyright 2006-2017 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.codegen.mybatis3;
+
+import java.util.List;
+
+import org.mybatis.generator.api.ProgressCallback;
+import org.mybatis.generator.codegen.AbstractJavaClientGenerator;
+import org.mybatis.generator.codegen.AbstractJavaGenerator;
+import org.mybatis.generator.codegen.mybatis3.javamapper.SimpleAnnotatedClientGenerator;
+import org.mybatis.generator.codegen.mybatis3.javamapper.SimpleJavaClientGenerator;
+import org.mybatis.generator.codegen.mybatis3.model.PrimaryKeyGenerator;
+import org.mybatis.generator.codegen.mybatis3.model.SimpleModelGenerator;
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.SimpleXMLMapperGenerator;
+import org.mybatis.generator.internal.ObjectFactory;
+
+/**
+ * generator type of "mybatis3simple"_DLC 
+ * 
+ * @author Belong.
+ * 
+ */
+public class IntrospectedTableMyBatis3SimpleImpl extends IntrospectedTableMyBatis3Impl {
+    public IntrospectedTableMyBatis3SimpleImpl() {
+        super();
+    }
+
+    @Override
+    protected void calculateXmlMapperGenerator(AbstractJavaClientGenerator javaClientGenerator, 
+            List<String> warnings,
+            ProgressCallback progressCallback) {
+        if (javaClientGenerator == null) {
+            if (context.getSqlMapGeneratorConfiguration() != null) {
+                xmlMapperGenerator = new SimpleXMLMapperGenerator();
+            }
+        } else {
+            xmlMapperGenerator = javaClientGenerator.getMatchedXMLGenerator();
+        }
+        
+        initializeAbstractGenerator(xmlMapperGenerator, warnings,
+                progressCallback);
+    }
+
+    @Override
+    protected AbstractJavaClientGenerator createJavaClientGenerator() {
+        if (context.getJavaClientGeneratorConfiguration() == null) {
+            return null;
+        }
+        
+        String type = context.getJavaClientGeneratorConfiguration()
+                .getConfigurationType();
+
+        AbstractJavaClientGenerator javaGenerator;
+        if ("XMLMAPPER".equalsIgnoreCase(type)) { //$NON-NLS-1$
+        	javaGenerator = new SimpleJavaClientGenerator();
+        } else if ("ANNOTATEDMAPPER".equalsIgnoreCase(type)) { //$NON-NLS-1$
+            javaGenerator = new SimpleAnnotatedClientGenerator();
+        } else if ("MAPPER".equalsIgnoreCase(type)) { //$NON-NLS-1$
+            javaGenerator = new SimpleJavaClientGenerator();
+        } else {
+            javaGenerator = (AbstractJavaClientGenerator) ObjectFactory
+                    .createInternalObject(type);
+        }
+
+        return javaGenerator;
+    }
+
+    //generator entity class and primaryKey class
+    @Override
+    protected void calculateJavaModelGenerators(List<String> warnings,
+            ProgressCallback progressCallback) {
+    	{
+    		AbstractJavaGenerator javaGenerator = new SimpleModelGenerator();
+        	initializeAbstractGenerator(javaGenerator, warnings,
+        			progressCallback);
+        	javaModelGenerators.add(javaGenerator);
+    	}
+    	//2018.2.2 add generate PrimaryKey class  function ----start
+        if (getRules().generatePrimaryKeyClass()) {
+            AbstractJavaGenerator javaGenerator = new PrimaryKeyGenerator();
+            initializeAbstractGenerator(javaGenerator, warnings,
+                    progressCallback);
+            javaModelGenerators.add(javaGenerator);
+        }
+        //2018.2.2 add generate PrimaryKey class  function ----end
+    }
+}
+
+
